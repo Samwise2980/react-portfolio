5fdbd627ee7b005b21d707b770daed73b2729f6c
Samwise <samuelfoster0@gmail.com>
2020-05-13 20:15:42 -0700

    Updates

---
+++ precache-manifest.93622ce14edb7eddaafb3a1e11464a2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c58af7530a37c04b3b7112c57e815241",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "78b3e089992ebcf0e8be",
+    "url": "/react-portfolio/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "2e03a8b68637b986993a",
+    "url": "/react-portfolio/static/css/main.0f1a729a.chunk.css"
+  },
+  {
+    "revision": "78b3e089992ebcf0e8be",
+    "url": "/react-portfolio/static/js/2.b20f1fbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.b20f1fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e03a8b68637b986993a",
+    "url": "/react-portfolio/static/js/main.56189880.chunk.js"
+  },
+  {
+    "revision": "37db3f26b7c0ea36aed0",
+    "url": "/react-portfolio/static/js/runtime-main.d08fb9a1.js"
+  }
+]);
